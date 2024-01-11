2d597ce72824200762210268cfa1d05a898864a1
Misha <mnelyubo@buffalo.edu>
2024-01-11 11:16:48 -0500

    switch to numeric output of control signals

--- Software/PlatformIO/desktop_validator_of/motor_control.cpp
+++ Software/PlatformIO/desktop_validator_of/motor_control.cpp
@@ -1,29 +1,17 @@
 #include "motor_control.hpp"
 
-#define L_OFF ctrl[0]=0; ctrl[1]=0;
-#define L_FWD ctrl[0]=0; ctrl[1]=1;
-#define L_BCK ctrl[0]=1; ctrl[1]=0;
-
-#define R_OFF ctrl[2]=0; ctrl[3]=0;
-#define R_FWD ctrl[2]=0; ctrl[3]=1;
-#define R_BCK ctrl[2]=1; ctrl[3]=0;
-
-
 int16_t rule(int u, int v){
   // magnitude squared, for computational simplicity of not taking square roots on a board that says they're 0
   int mag = u*u + v*v;
   return mag >= OFIT * OFIT;
 }
 
-void motorControl(int16_t U[IMAGE_ROWS * IMAGE_COLS], int16_t V[IMAGE_ROWS * IMAGE_COLS], int ctrl[4]){
+void motorControl(int16_t U[IMAGE_ROWS * IMAGE_COLS], int16_t V[IMAGE_ROWS * IMAGE_COLS], int ctrl[2]){
   // Parameters: 
   //   U optical flow component, 
   //   V optical flow component, 
   //   motor control output signals (0 Left, 1 Right)
   
-  // initially, keep the signals on
-  L_FWD; R_FWD;
-
   // count flow in each half of the screen
   int leftSum = 0;
   int rightSum = 0;
@@ -41,6 +29,9 @@ void motorControl(int16_t U[IMAGE_ROWS * IMAGE_COLS], int16_t V[IMAGE_ROWS * IMA
   // Serial.printf("L: %d\tR:%d\t", leftSum, rightSum);
 
   // turn off the motor on the OTHER side to get the bot to turn away from that
-  if(leftSum > OFQT) {R_OFF;}
-  if(rightSum > OFQT) {L_OFF;}
+  // with no flow, go full forward.  
+  // The more flow, the more you slow down.  
+  // At over 100 collisions, reverse.
+  if(leftSum > OFQT) {ctrl[1] = 100 - leftSum;}   
+  if(rightSum > OFQT) {ctrl[0] = 100 - rightSum;}
 }
--- Software/PlatformIO/desktop_validator_of/motor_control.hpp
+++ Software/PlatformIO/desktop_validator_of/motor_control.hpp
@@ -14,9 +14,11 @@ using namespace std;
 /**
  * U - the U (-1 Left, 0 - stationary, +1 Right) optical flow values of all pixels
  * V - the V (-1 Down, 0 - stationary, +1 Up)    optical flow values of all pixels
- * ctrl - the control signals to send to the motors. 0,1 - left motor.  2,3 - right motor.
-*/
-void motorControl(int16_t U[IMAGE_ROWS * IMAGE_COLS], int16_t V[IMAGE_ROWS * IMAGE_COLS], int ctrl[4]);
+ * ctrl - the control signals to send to the motors. 
+ *          Entry at index: 0 - left motor.  1 - right motor.
+ *          Values: 0 - off. 100 - fully forward.  -100 - fully backward.
+ */
+void motorControl(int16_t U[IMAGE_ROWS * IMAGE_COLS], int16_t V[IMAGE_ROWS * IMAGE_COLS], int ctrl[2]);
 
 /**
  * The rule for whether or not a flow point has breached the critical threshold for motion to be counted torward the limit at which a motor should stop
