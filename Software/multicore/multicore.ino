bb805fa5f5f6f5336bea472ee54f0b85b5023664
Misha <mnelyubo@buffalo.edu>
2024-01-14 17:50:19 -0500

    working with fixed duty values

--- Software/multicore/multicore.ino
+++ Software/multicore/multicore.ino
@@ -7,8 +7,6 @@
 TaskHandle_t motorTask;
 int ticks = 0;
 
-void powerCycle(int dL = 100, int dR = 100);
-
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting up");
@@ -22,34 +20,32 @@ void motorSetup(){
     digitalWrite(motor_pins[i], LOW);  // initialize all 4 pins to off
   }
 
-  int duties[] = {50, 50};
-
   // https://randomnerdtutorials.com/esp32-dual-core-arduino-ide/
   xTaskCreatePinnedToCore(
-    core0motorControl,  /* Task function. */
-    "motorTask",        /* name of task. */
-    10000,              /* Stack size of task */
-    &duties,            /* parameter of the task */
-    1,                  /* priority of the task */
-    &motorTask,         /* Task handle to keep track of created task */
-    0);                 /* pin task to core 0 */
+    core0motorControl,   /* Task function. */
+    "motorTask",     /* name of task. */
+    10000,       /* Stack size of task */
+    NULL,        /* parameter of the task */
+    1,           /* priority of the task */
+    &motorTask,      /* Task handle to keep track of created task */
+    0);          /* pin task to core 0 */
 }
 
 //core0motorControl: blinks an LED every 1000 ms
 void core0motorControl( void * pvParameters ){
-    int *duties = (int *) pvParameters;
-    int dL = duties[0];
-    int dR = duties[1];
+  for(;;){
     Serial.printf("motorTask (t=%d) running on core %d [ticks = %d]\n", millis(), xPortGetCoreID(), ticks);
-    powerCycle(dL, dR);
-    vTaskDelete(motorTask);
+    powerCycle();
+  }
 }
 
 
 void loop() {}
 
 
-void powerCycle(int dL, int dR){
+void powerCycle(){
+  int dL = 100;
+  int dR = 100;
 
   auto Lpin = D1;
   auto Rpin = D3;
@@ -69,7 +65,7 @@ void powerCycle(int dL, int dR){
     digitalWrite(Lpin, !!(stopL > t));  // power the motor if the stop time for that motor hasn't passed yet
     digitalWrite(Rpin, !!(stopR > t));  // power the motor if the stop time for that motor hasn't passed yet
     delay(1);             // 1ms pause as this is the lowest coherent unit of time measurement for the board
+    // motors->state++;
     ticks++;
   }
 }
-
