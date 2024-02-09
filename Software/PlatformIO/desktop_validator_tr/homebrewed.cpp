b92e74caad84c9c844eb920bbe7319729646c93b
Misha <mnelyubo@buffalo.edu>
2024-02-09 15:42:03 -0500

    basic camera echo

--- Software/PlatformIO/desktop_validator_tr/homebrewed.cpp
+++ Software/PlatformIO/desktop_validator_tr/homebrewed.cpp
@@ -3,8 +3,36 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/video.hpp>
+#include <iostream>
+#include <stdio.h>
+
+using namespace cv;
 
 int main(){
-    printf("asdf\n");
+    VideoCapture capture(0); 
+
+    if (!capture.isOpened()){
+        //error in opening the video input
+        printf("Unable to open file!");
+        return 3;
+    } else {
+        printf("Starting camera feed!");
+    }
+
+    while (true) {
+        Mat sourceImage;
+        capture >> sourceImage;
+        if(sourceImage.empty()){
+            printf("End of capture stream");
+            break;
+        }
+
+        imshow("camera", sourceImage);
+
+        int keyboard = waitKey(30);
+        if (keyboard == 'q' || keyboard == 27)
+            break;
+
+    }
     return 0;
 }
