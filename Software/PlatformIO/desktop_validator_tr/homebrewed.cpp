fe542a0a462e54a96616815d7e54423d2444bcbc
Misha <mnelyubo@buffalo.edu>
2024-02-09 15:32:31 -0500

    restart at base

---
+++ Software/PlatformIO/desktop_validator_tr/homebrewed.cpp
@@ -0,0 +1,10 @@
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/video.hpp>
+
+int main(){
+    printf("asdf\n");
+    return 0;
+}
