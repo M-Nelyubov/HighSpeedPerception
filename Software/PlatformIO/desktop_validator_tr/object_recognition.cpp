967322c7cb63c2e368455420ddfc1c2e5b4d0c8d
Misha <mnelyubo@buffalo.edu>
2024-02-09 16:15:25 -0500

    wrote red detector

--- Software/PlatformIO/desktop_validator_tr/object_recognition.cpp
+++ Software/PlatformIO/desktop_validator_tr/object_recognition.cpp
@@ -1,5 +1,21 @@
 #include "object_recognition.hpp"
 
-void extractRed(uint8_t inputFrame [IMAGE_SIZE * COLOR_CHANNELS], uint8_t outputFrame [IMAGE_SIZE]){
+#define RED_CHANNEL_IDX 2
+#define RST 1.2f // red significance threshold - how much red has to be greater than the pixel brightness magnitude to count as significant
 
+void extractRed(uint8_t inputFrame [IMAGE_SIZE * COLOR_CHANNELS], uint8_t outputFrame [IMAGE_SIZE]){
+    for(int i=0; i<IMAGE_SIZE; i++){
+        int ii = i * COLOR_CHANNELS;  // input index
+        int oi = i;                   // ouptut index
+
+        // Calculate greyscale pixel value
+        int mag = 0;
+        for(int c=0;c<COLOR_CHANNELS;c++){
+            mag += inputFrame[ii+c];
+        }
+        mag /= COLOR_CHANNELS;
+        int red = inputFrame[ii + RED_CHANNEL_IDX];
+
+        outputFrame[oi] = (red > RST * mag) ? (red - RST * mag) : 0;
+    }
 }
