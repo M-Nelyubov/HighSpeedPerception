257dea4d3c3a253113f81e3fedc9d7e1a50f9edc
Misha <mnelyubo@buffalo.edu>
2024-02-28 15:25:11 -0500

    auto-detect serial port

--- Software/PlatformIO/RedBallChaser/test/serial_plotter.py
+++ Software/PlatformIO/RedBallChaser/test/serial_plotter.py
@@ -1,13 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import serial
+import serial.tools.list_ports
 import time
+
 
 BUFFER_SIZE = 100
 t0 = time.time()
 
 print("Starting COM")
-com = serial.Serial("COM9",baudrate=115200)
+# Find the available port
+# https://pyserial.readthedocs.io/en/latest/tools.html#module-serial.tools.list_ports
+# https://pyserial.readthedocs.io/en/latest/tools.html#serial.tools.list_ports.ListPortInfo
+ports = [port for port, desc, info in serial.tools.list_ports.comports()]  
+if len(ports) == 0:
+    print("No devices available.")
+    exit
+    
+if len(ports) > 1:
+    print("Multiple devices detected:", ports)
+
+print("Connecting to device:", ports[0])
+com = serial.Serial(ports[0], baudrate=115200)
 print("Started COM")
 
 buffers = {}
@@ -23,6 +37,9 @@ while True:
 
     print(f"Read components: {components}")
 
+    if len(components) == 0:
+        continue
+
     t[i] = time.time() - t0
 
     for c in components:
@@ -37,7 +54,8 @@ while True:
         
         buffers[label][i] = val
 
-        plt.scatter(t, buffers[label], label=label)#, s=2)
+        plt.scatter(buffers[label], t, label=label)#, s=2)
+        plt.xlim([-1,1])
 
     plt.legend()
     plt.grid()
