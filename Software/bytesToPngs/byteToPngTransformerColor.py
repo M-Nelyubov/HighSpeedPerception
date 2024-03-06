1c74163d7cf42c560221f4b4dc7ae6187244f10a
Misha <mnelyubo@buffalo.edu>
2024-03-06 16:37:16 -0500

    non-square image support

--- Software/bytesToPngs/byteToPngTransformerColor.py
+++ Software/bytesToPngs/byteToPngTransformerColor.py
@@ -2,14 +2,15 @@ import numpy as np
 import os
 import cv2
 
-COLS, ROWS = (96, 96)
+COLS, ROWS = (320, 240)
 ratio = COLS/ROWS
 
-DISPLAY_ROWS = 400
+DISPLAY_ROWS = int(240 * 2)
 DISPLAY_COLS = int(ratio * DISPLAY_ROWS)
+print(f"c{DISPLAY_COLS},{COLS} - r{DISPLAY_ROWS},{ROWS}")
 
 base = "D:/raw/img"
-base = "C:/Users/maste/Pictures/esp32/sequences/96x96/redTargetting1"
+# base = "C:/Users/maste/Pictures/esp32/sequences/96x96/redTargetting1"
 files = os.listdir(base)
 
 def relu(x):
@@ -28,7 +29,7 @@ def radialPercent(image, x,y,r):
     # print(np.sum(image))
     for x in range(lX, uX):
         for y in range(uY, lY):
-            if image[y,x] > 0:
+            if image[x,y] > 0:
                 s+=1
 
     p = s / ((uX - lX) * (lY - uY))
@@ -46,6 +47,7 @@ while True:
     with open(fullPath, "rb") as imageBytes:
         bytes = bytearray(imageBytes.read())
         arr = np.array(bytes, dtype="uint8")
+        print(f"Read {file} length: {len(arr)}")
         grid = arr.reshape((ROWS,COLS,3))
         # grid = cv2.resize(grid, (ROWS//4, COLS//4))
         grid = cv2.resize(grid, (DISPLAY_ROWS, DISPLAY_COLS))
@@ -59,7 +61,7 @@ while True:
         print(f"R min:{np.min(rs)} max:{np.max(rs)} avg:{np.mean(rs)}", end="\t")
         print(f"G min:{np.min(gs)} max:{np.max(gs)}", end="\t")
         print(f"B min:{np.min(bs)} max:{np.max(bs)}", end="\t")
-        print("Rmap shape=", rmap.shape, "Rmap avg=", np.mean(rmap), "Rmap sum=", np.sum(rmap), end="\t")
+        print(f"Rmap shape=", rmap.shape, "Rmap avg=", np.mean(rmap), "Rmap sum=", np.sum(rmap), end="\t")
 
         print("")
         # Generate cmap
@@ -69,55 +71,66 @@ while True:
 
         magnitude_threshold = 1000
         if np.sum(rmap) > magnitude_threshold:
+            # Feature flags
+            computeX = True
+            computeY = True
+            drawCenter = True
+            drawRectangle = True
 
-            # Compute X Mean
-            sumsX = np.zeros(DISPLAY_COLS)
-            xfx = np.zeros(DISPLAY_COLS)
-            for c in range(DISPLAY_COLS):
-                sumsX[c] = np.sum(rmap[:,c])
-                xfx[c] = sumsX[c] * c
-            meanX = int(np.sum(xfx) / (.0001+ np.sum(sumsX)))
+            if computeX:
+                # Compute X Mean
+                sumsX = np.zeros(DISPLAY_COLS)
+                xfx = np.zeros(DISPLAY_COLS)
+                # print(f"Iterating 0..{DISPLAY_COLS}")
+                for c in range(DISPLAY_COLS):
+                    sumsX[c] = np.sum(rmap[c,:])
+                    xfx[c] = sumsX[c] * c
+                meanX = int(np.sum(xfx) / (.0001+ np.sum(sumsX)))
 
-            # Compute Y Mean
-            sumsY = np.zeros(DISPLAY_COLS)
-            yfy = np.zeros(DISPLAY_COLS)
-            for r in range(DISPLAY_ROWS):
-                sumsY[r] = np.sum(rmap[r,:])
-                yfy[r] = sumsY[r] * r
-            meanY = int(np.sum(yfy) / (.0001+ np.sum(sumsY)))
+            if computeY:
+                # Compute Y Mean
+                sumsY = np.zeros(DISPLAY_COLS)
+                yfy = np.zeros(DISPLAY_COLS)
+                for r in range(DISPLAY_ROWS):
+                    sumsY[r] = np.sum(rmap[:,r])
+                    yfy[r] = sumsY[r] * r
+                meanY = int(np.sum(yfy) / (.0001+ np.sum(sumsY)))
 
-            # Draw the green line
-            cmap[:,meanX,1] = 255
-            cmap[meanY,:,1] = 255
+            if drawCenter:
+                # Draw the green line
+                cmap[meanX,:,1] = 255
+                cmap[:,meanY,1] = 255
 
-            r = 90 # 5 pixel buffer around the image
-            p = 0
-            explored = {}
-            # p == 0 and
-            while r not in explored and r > 0:   # assumes concave distribution of p across argument r
-                # normalizing p by the expected portion for an ideal circle, the p acts as an error signal
-                p = radialPercent(rmap, meanX, meanY, r) - np.pi / 4
-                explored[r] = p
-                # print(f"rad {r} -> {p}")
-                if p > 0:
-                    r+=1
-                if p < 0:
-                    r-=1
+            if drawRectangle:
+                r = 20 # pixel buffer around the center of the circle
+                p = 0
+                explored = {}
+                # p == 0 and
+                while r not in explored and r > 0 and r < DISPLAY_ROWS:   # assumes concave distribution of p across argument r
+                    # normalizing p by the expected portion for an ideal circle, the p acts as an error signal
+                    # tolerance - some additional factor of acceptable degradation beyond an ideal circle. [0-1]
+                    tolerance = 0.6
+                    p = radialPercent(rmap, meanX, meanY, r) - (np.pi / 4) * tolerance
+                    explored[r] = p
+                    # print(f"rad {r} -> {p}")
+                    if p > 0:
+                        r+=1
+                    if p < 0:
+                        r-=1
 
-            # Draw the bounding box
-            upperY = max(0, meanY - r)
-            lowerY = min(DISPLAY_ROWS, meanY + r)
-            lowerX = max(0, meanX - r)
-            upperX = min(DISPLAY_COLS, meanX + r)
-            # cmap[:,lowerX,2] = 255
-            # cmap[:,upperX,2] = 255
-            for x in range(lowerX, upperX):
-                for y in range(upperY, lowerY):
-                    cmap[y,x,1] = 128
+                # Draw the bounding box
+                upperY = max(0, meanY - r)
+                lowerY = min(DISPLAY_ROWS, meanY + r)
+                lowerX = max(0, meanX - r)
+                upperX = min(DISPLAY_COLS, meanX + r)
+                # cmap[:,lowerX,2] = 255
+                # cmap[:,upperX,2] = 255
+                for x in range(lowerX, upperX):
+                    for y in range(upperY, lowerY):
+                        cmap[x,y,1] = 128
 
         # End of the big if - rendering segment
 
-        print(f"Read length: {file}")
         # img = cv2.imdecode(grid, cv2.IMREAD_UNCHANGED) # https://www.geeksforgeeks.org/python-opencv-imdecode-function/
         cv2.imshow("frame", grid)
         cv2.imshow("b", bs.astype("uint8"))
